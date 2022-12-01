4113867c153d117f09a2e8360cc7eef6ebb888a2
kbcruz6 <agustintcruz@gmail.com>
2022-12-01 11:13:44 -0300

    wp

--- src/components/SocialMediaBottom.jsx
+++ src/components/SocialMediaBottom.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { FiPaperclip } from "react-icons/fi";
+import { BsWhatsapp } from "react-icons/bs";
 
 import Logo from "../assets/logo1.png";
 import { Link } from "react-scroll";
@@ -12,6 +13,7 @@ const SocialMediaBottom = () => {
       {/*//! Social media horizontal  */}
       <div className="lg:hidden">
         <ul className="flex fixed flex-row sm:bottom-0 left-[35%] minsm:top-0 minsm:left-[20%]">
+          {/*//! LinkedIn  */}
           <li className="w-[60px] h-[50px] flex justify-between items-center duration-300 bg-blue-600 hover:scale-125">
             <a
               href="https://www.linkedin.com/in/agustin-frontend-javascript/"
@@ -21,6 +23,7 @@ const SocialMediaBottom = () => {
               <FaLinkedin size={30} />
             </a>
           </li>{" "}
+          {/*//! GitHub  */}
           <li className="w-[60px] h-[50px] flex justify-between items-center duration-300 bg-[#333333] hover:scale-125">
             <a
               href="https://github.com/kbcruz6"
@@ -30,6 +33,7 @@ const SocialMediaBottom = () => {
               <FaGithub size={30} />
             </a>
           </li>{" "}
+          {/*//! Email  */}
           <li className="w-[60px] h-[50px] flex justify-between items-center duration-300 bg-[#61ab9b] hover:scale-125">
             <a
               href="mailto:agustin.tcw@gmail.com"
@@ -39,12 +43,23 @@ const SocialMediaBottom = () => {
               <HiOutlineMail size={30} />
             </a>
           </li>{" "}
+          {/*//! CV  */}
           <li className="w-[60px] h-[50px] flex justify-between items-center duration-300 bg-[#565f69] hover:scale-125">
             <a
               href="https://github.com/kbcruz6/kbcruz6.github.io/raw/main/CvAgustinCruz/CV%20Agustin%20Cruz.pdf"
               className="flex justify-between items-center w-full text-gray-300"
             >
               <FiPaperclip size={30} />
+            </a>
+          </li>
+          {/*//! Phone  */}
+          <li className="w-[60px] h-[50px] flex justify-between items-center duration-300 bg-[#25D366] hover:scale-125">
+            <a
+              href="http://api.whatsapp.com/send?phone=541122552730"
+              className="flex justify-between items-center w-full text-white"
+              target="_blank"
+            >
+              <BsWhatsapp size={30} />
             </a>
           </li>
         </ul>
