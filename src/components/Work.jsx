efa0dd3aec30bb253a66a2e2d08a316748b54e81
kbcruz6 <agustintcruz@gmail.com>
2022-12-24 16:49:31 -0300

    mern added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,6 +7,7 @@ import nextJs from "../assets/nextJsApp.png";
 import currentPortfolio from "../assets/currentPortfolio.png";
 import weather from "../assets/weather.png";
 import { FaEye, FaGithub } from "react-icons/fa";
+import books from "../assets/books.png";
 
 const Work = () => {
   return (
@@ -27,8 +28,39 @@ const Work = () => {
         {/*//! Card grid container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/*//! Card grid item */}
+          {/*//! BOOKS MERN APP*/}
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="100"
+            className="max-w-sm rounded-md shadow-lg shadow-[#040c16] p-2 bg-[#11294e]/50 hover:bg-[#11294e]/90"
+          >
+            <div
+              style={{ backgroundImage: `url(${books})` }}
+              className="group container rounded-md flex justify-center items-center mx-auto content-div1"
+            >
+              {/*//! Hover effects */}
+              <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center w-full h-full p-0 content-div rounded-md duration-300">
+                <a href="https://books-info.vercel.app/" target="_blank">
+                  <button className="text-center rounded-full px-5 py-3 m-2 bg-white text-gray-700 font-bold text-lg btn shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] ease-out hover:translate-y-1  transition-all">
+                    <FaEye size={30} />
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/stars/kbcruz6/lists/books-mern-app"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-full px-5 py-3 m-2 bg-white text-gray-700 font-bold text-lg btn shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] ease-out hover:translate-y-1  transition-all">
+                    <FaGithub size={30} />
+                  </button>
+                </a>
+              </div>
+            </div>
+            <div className="font-bold text-xl my-2 flex justify-center items-center">
+              MERN app (w/MySQL)
+            </div>
+          </div>
           {/*//! WEATHER APP*/}
-          {/*//?------------------------------------------------  */}
           <div
             data-aos="fade-up"
             data-aos-duration="1000"
@@ -61,41 +93,6 @@ const Work = () => {
             </div>
             <div className="font-bold text-xl my-2 flex justify-center items-center">
               NextJS Weather App
-            </div>
-          </div>
-          {/*//?------------------------------------------------  */}
-          {/*//! REACT APPS*/}
-          <div
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            data-aos-delay="300"
-            className="max-w-sm rounded-md shadow-lg shadow-[#040c16] p-2  bg-[#11294e]/50 hover:bg-[#11294e]/90 duration-300"
-          >
-            <div
-              style={{ backgroundImage: `url(${reactApps})` }}
-              className="group container rounded-md flex justify-center items-center mx-auto content-div1"
-            >
-              {/*//! Hover effects */}
-              <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center w-full h-full p-0 content-div rounded-md">
-                <div className="flex text-center justify-start">
-                  <a href="https://kbcruz6.github.io/react/" target="_blank">
-                    <button className="text-center rounded-full px-5 py-3 m-2 bg-white text-gray-700 font-bold text-lg btn shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] ease-out hover:translate-y-1  transition-all">
-                      <FaEye size={30} />
-                    </button>
-                  </a>
-                  <a
-                    href="https://github.com/kbcruz6/kbcruz6.github.io/tree/main/react"
-                    target="_blank"
-                  >
-                    <button className="text-center rounded-full px-5 py-3 m-2 bg-white text-gray-700 font-bold text-lg btn shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] ease-out hover:translate-y-1  transition-all">
-                      <FaGithub size={30} />
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>{" "}
-            <div className="font-bold text-xl my-2 flex justify-center items-center">
-              React Apps
             </div>
           </div>
           {/*//! WEB CV */}
@@ -204,7 +201,41 @@ const Work = () => {
               Current Portfolio{" "}
             </div>
           </div>
-          {/*//! PURE JS */}
+          {/*//! REACT APPS*/}
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="1000"
+            className="max-w-sm rounded-md shadow-lg shadow-[#040c16] p-2  bg-[#11294e]/50 hover:bg-[#11294e]/90 duration-300"
+          >
+            <div
+              style={{ backgroundImage: `url(${reactApps})` }}
+              className="group container rounded-md flex justify-center items-center mx-auto content-div1"
+            >
+              {/*//! Hover effects */}
+              <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center w-full h-full p-0 content-div rounded-md">
+                <div className="flex text-center justify-start">
+                  <a href="https://kbcruz6.github.io/react/" target="_blank">
+                    <button className="text-center rounded-full px-5 py-3 m-2 bg-white text-gray-700 font-bold text-lg btn shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] ease-out hover:translate-y-1  transition-all">
+                      <FaEye size={30} />
+                    </button>
+                  </a>
+                  <a
+                    href="https://github.com/kbcruz6/kbcruz6.github.io/tree/main/react"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-full px-5 py-3 m-2 bg-white text-gray-700 font-bold text-lg btn shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] ease-out hover:translate-y-1  transition-all">
+                      <FaGithub size={30} />
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>{" "}
+            <div className="font-bold text-xl my-2 flex justify-center items-center">
+              React Apps
+            </div>
+          </div>
+          {/* //! PURE JS
           <div
             data-aos="fade-up"
             data-aos-duration="1000"
@@ -215,7 +246,7 @@ const Work = () => {
               style={{ backgroundImage: `url(${pureJs})` }}
               className="group container rounded-md flex justify-center items-center mx-auto content-div1"
             >
-              {/*//! Hover effects */}
+              //! Hover effects 
               <div className="opacity-0 group-hover:opacity-100 flex justify-center items-center w-full h-full p-0 content-div rounded-md">
                 <div className="flex text-center justify-start">
                   <a
@@ -239,37 +270,6 @@ const Work = () => {
             </div>{" "}
             <div className="font-bold text-xl my-2 flex justify-center items-center">
               Pure JavaScript{" "}
-            </div>
-          </div>
-          {/*//! OLD PORTFOLIO 
-           <div
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            data-aos-delay="1400"
-            style={{ backgroundImage: `url(${oldPortfolio})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div1"
-          >
-            //! Hover effects
-            <div className="opacity-0 group-hover:opacity-100 grid grid-rows-2 justify-center items-center w-full h-full p-0 content-div rounded-md">
-              <span className="text-center font-bold text-2xl opacity-100 text-white pt-5">
-                Old Portfolio
-              </span>
-
-              <div className="flex text-center justify-start align-top pb-8">
-                <a href="https://kbcruz6.github.io/" target="_blank">
-                  <button className="text-center rounded-full px-5 py-3 m-2 bg-white text-gray-700 font-bold text-lg btn shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] ease-out hover:translate-y-1  transition-all">
-                    <FaEye size={30} />
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/kbcruz6/kbcruz6.github.io"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-full px-5 py-3 m-2 bg-white text-gray-700 font-bold text-lg btn shadow-[0_6px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] ease-out hover:translate-y-1  transition-all">
-                    <FaGithub size={30} />
-                  </button>
-                </a>
-              </div>
             </div>
           </div> */}
         </div>
